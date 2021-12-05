59e652d44829ce19c7f68f01a1490ec562073eba
Diarmuid Collins <diarmuid.m.collins@gmail.com>
2021-12-05 08:24:51 +0000

    Working part 2

--- day5.py
+++ day5.py
@@ -1,5 +1,8 @@
 import re
+import operator
+import logging
 
+logging.basicConfig(level=logging.INFO)
 
 DIR_X = "x"
 DIR_Y = "y"
@@ -12,7 +15,7 @@ class Line(object):
         self.y1 = y1
         self.y2 = y2
 
-    def to_points(self) -> dict:
+    def to_points(self, diagonal: bool) -> dict:
         _points = {}
         if self.x1 == self.x2:
             _points[self.x1] = {}
@@ -36,6 +39,33 @@ class Line(object):
                     _points[x][self.y1] = 1
             else:
                 raise Exception("Did not expect this config(x) {}".format(repr(self)))
+
+        if diagonal and (self.x1 != self.x2 and self.y1 != self.y2):
+            logging.debug("Adding a diagonal {}".format(self))
+            if abs(self.x1 - self.x2) != abs(self.y1 - self.y2):
+                raise Exception("Diagonals should have matching lengths. {}".format(repr(self)))
+            delta = abs(self.x1 - self.x2)
+            if self.x1 > self.x2:
+                x_operator = operator.sub
+            else:
+                x_operator = operator.add
+            if self.y1 > self.y2:
+                y_operator = operator.sub
+            else:
+                y_operator = operator.add
+
+            for offset in range(delta+1):
+                x_point = x_operator(self.x1, offset)
+                y_point = y_operator(self.y1, offset)
+                #logging.debug("Diagonal x={} y={}".format(x_point, y_point))
+                if x_point < 0 or y_point < 0:
+                    raise Exception("Should not have negative points. Delta={} Line={}".format(delta, repr(self)))
+                if x_point not in _points:
+                    _points[x_point] = {}
+                    _points[x_point][y_point] = 1
+                else:
+                    _points[x_point][y_point] += 1
+
         return _points
 
     def __repr__(self):
@@ -45,14 +75,17 @@ class Line(object):
 class Matrix(object):
     def __init__(self, max_x: int, max_y: int) -> None:
         self._matrix = {}
-        for x in range(max_x):
+        self._max_x = max_x
+        self._max_y = max_y
+        for x in range(max_x+1):
             self._matrix[x] = {}
-            for y in range(max_y):
+            for y in range(max_y+1):
                 self._matrix[x][y] = 0
 
-    def add_line(self, line: Line) -> bool:
+    def add_line(self, line: Line, diagonal=False) -> bool:
         """Add a line to the matrix"""
-        line_points = line.to_points()
+        line_points = line.to_points(diagonal)
+        logging.debug("Adding Line {} to matrix {}".format(line, self))
         for x, ydir in line_points.items():
             for y, val in ydir.items():
                 self._matrix[x][y] += val
@@ -68,6 +101,9 @@ class Matrix(object):
 
         return _count
 
+    def __repr__(self):
+        return "MatrixMaxX={} MatrixMaxY={}".format(self._max_x, self._max_y)
+
 
 def max(lines: [Line], dir) -> int:
     """Get the max coordinates in  a list of lines"""
@@ -75,8 +111,12 @@ def max(lines: [Line], dir) -> int:
     for l in lines:
         if dir == DIR_X and l.x2 > _max:
             _max = l.x2
+        elif dir == DIR_X and l.x1 > _max:
+            _max = l.x1
         elif dir == DIR_Y and l.y2 > _max:
             _max = l.y2
+        elif dir == DIR_Y and l.y1 > _max:
+            _max = l.y1
     return _max
 
 
@@ -93,8 +133,17 @@ for l in f.readlines():
 max_x = max(alllines, DIR_X)
 max_y = max(alllines, DIR_Y)
 
+#part 1
 matrix = Matrix(max_x, max_y)
 for mline in alllines:
     matrix.add_line(mline)
 
 print("Danger points={}".format(matrix.get_danger(2)))
+assert matrix.get_danger(2) == 3990, "Wrong answer"
+
+# part 2
+matrixp = Matrix(max_x, max_y)
+for mline in alllines:
+    matrixp.add_line(mline, diagonal=True)
+
+print("Danger points={}".format(matrixp.get_danger(2)))
