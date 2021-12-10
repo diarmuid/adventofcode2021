8405d6efdc83b64581adb867da9bcc870173b8c0
Diarmuid Collins <diarmuid.m.collins@gmail.com>
2021-12-10 07:59:14 +0000

    minor updates

--- day10.py
+++ day10.py
@@ -1,24 +1,23 @@
 import RealInput
 import re
 import logging
+from functools import reduce
 
 
-#brks = re.compile(r"((?:\[\])|(?:\(\))|(?:{})|(?:<>))")
 brks = re.compile(r"(\[\]|\(\)|{}|<>)")
 illegal_bkts = re.compile(r"(\[}|\[\)|\[>|"
                           r"{\]|{\)|{>|"
                           r"\(\]|\(}|\(>|"
                           r"<\]|<\)|<})")
 COSTS = {")": 3, "]": 57, "}": 1197, ">": 25137}
-CORRECTION_COST = {"(": 1, "[": 2, "{": 3, "<": 4}
+CORRECTION_COST = {"(": 1, "[": 2, "{": 3, "<": 4}  # reversed from puzzle to make replacement easier
 
 running_cost = 0
 replacement_scores = []
 
-for i, l in enumerate(RealInput.RealInput.DAY10.splitlines()):
+for i, modified_line in enumerate(RealInput.RealInput.DAY10.splitlines()):
     keep_iterating = True
-    modified_line = l
-    incomplete = True
+    incomplete_line = True
     while keep_iterating:
         m = illegal_bkts.search(modified_line)
         if m:
@@ -26,14 +25,15 @@ for i, l in enumerate(RealInput.RealInput.DAY10.splitlines()):
             logging.debug("{}:{}:{}:{}".format(i, m.start(), m.group(), COSTS[closeb] * m.start()))
             running_cost += COSTS[closeb]
             keep_iterating = False
-            incomplete = False
+            incomplete_line = False
         else:
+            # Trim out the matching brackets until none are remaining
             (modified_line, subsmade) = brks.subn("", modified_line)
             if subsmade == 0:
                 keep_iterating = False
     # Part 2
-    if len(modified_line) > 0 and incomplete:
-        brks_to_add = reversed(list(modified_line))
+    if len(modified_line) > 0 and incomplete_line:  # incomplete line
+        brks_to_add = reversed(list(modified_line))  # add in reverse order
         fix_score = 0
         for addition in brks_to_add:
             fix_score *= 5
