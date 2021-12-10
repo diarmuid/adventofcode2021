4e019007e8bab4c38b1da31e48fac5b3f24e2b15
Diarmuid Collins <diarmuid.m.collins@gmail.com>
2021-12-10 07:51:25 +0000

    p2 complete

--- day10.py
+++ day10.py
@@ -10,12 +10,15 @@ illegal_bkts = re.compile(r"(\[}|\[\)|\[>|"
                           r"\(\]|\(}|\(>|"
                           r"<\]|<\)|<})")
 COSTS = {")": 3, "]": 57, "}": 1197, ">": 25137}
-running_cost = 0
+CORRECTION_COST = {"(": 1, "[": 2, "{": 3, "<": 4}
 
+running_cost = 0
+replacement_scores = []
 
 for i, l in enumerate(RealInput.RealInput.DAY10.splitlines()):
     keep_iterating = True
     modified_line = l
+    incomplete = True
     while keep_iterating:
         m = illegal_bkts.search(modified_line)
         if m:
@@ -23,9 +26,24 @@ for i, l in enumerate(RealInput.RealInput.DAY10.splitlines()):
             logging.debug("{}:{}:{}:{}".format(i, m.start(), m.group(), COSTS[closeb] * m.start()))
             running_cost += COSTS[closeb]
             keep_iterating = False
+            incomplete = False
         else:
             (modified_line, subsmade) = brks.subn("", modified_line)
             if subsmade == 0:
                 keep_iterating = False
-print(running_cost)
+    # Part 2
+    if len(modified_line) > 0 and incomplete:
+        brks_to_add = reversed(list(modified_line))
+        fix_score = 0
+        for addition in brks_to_add:
+            fix_score *= 5
+            fix_score += CORRECTION_COST[addition]
+        replacement_scores.append(fix_score)
+
+replacement_scores = sorted(replacement_scores)
+middle = replacement_scores[len(replacement_scores)//2]
+
+print(middle)
 assert running_cost == 388713, "wrong p1"
+#assert running_cost == 26397, "wrong p1"
+assert middle == 3539961434, "wrongp2"
