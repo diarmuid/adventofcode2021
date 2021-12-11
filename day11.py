759e968c2d17a424e64ab921f9195a15a42580ca
Diarmuid Collins <diarmuid.m.collins@gmail.com>
2021-12-11 10:38:54 +0000

    messin

--- day11.py
+++ day11.py
@@ -2,6 +2,7 @@ import logging
 from functools import reduce
 import RealInput
 from collections import namedtuple
+import itertools
 from math import prod
 
 
@@ -48,30 +49,27 @@ flash_count = 0
 all_flash = None
 step = 0
 simulate = True
+
 while simulate:
     keep_iterating = True
-    iteration_step = 0
+    # Increment all points
+    for _y in input_as_matrix.values():
+        _y.update({k: v+1 for k, v in _y.items()})
 
     while keep_iterating:
         keep_iterating = False  # Unless we find more octupses to flash
         for _x in range(x_max):
             for _y in range(y_max):
-                # First pass just increment everything
-                if iteration_step == 0:
-                    input_as_matrix[_y][_x] = (input_as_matrix[_y][_x] + 1)
-                    keep_iterating = True
                 # subsequent passes, increment anything touch a 10 or greater
                 # And reduce the flashes to 0 so they won't have a further impact
-                elif input_as_matrix[_y][_x] >= 10:
+                if input_as_matrix[_y][_x] >= 10:
                     for pt in get_locality(input_as_matrix, Pt(_x,_y)):
                         if input_as_matrix[pt.y][pt.x] != 0:
                             input_as_matrix[pt.y][pt.x] = (input_as_matrix[pt.y][pt.x] + 1)
                     input_as_matrix[_y][_x] = 0
                     keep_iterating = True
         # Debug
-        logging.debug("iteration {}".format(iteration_step))
         matrix_to_str(input_as_matrix)
-        iteration_step += 1
 
     # Count the flashes in this step
     new_flashes = 0
@@ -90,7 +88,7 @@ while simulate:
         sum_flashes += reduce(lambda a,b : a + b, list(y.values()))
 
     logging.debug("step={} flash={}".format(step+1, sum_flashes))
-    # Exit 
+    # Exit
     if sum_flashes == 0:
         print("P2={}".format(step+1))
         simulate = False
