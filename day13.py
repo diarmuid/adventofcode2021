85bebd14a38b6f8a9e4989c624eecf8d7c5ef327
Diarmuid Collins <diarmuid.m.collins@gmail.com>
2021-12-13 08:49:49 +0000

    comments

--- day13.py
+++ day13.py
@@ -33,9 +33,11 @@ def print_matrix(lol, maxx, maxy):
 data_in = RealInput.RealInput.DAY13
 
 input_as_matrix = defaultdict(dict)
-folds = []
+folds = []  # List of the folds to perform
 max_x = 0
 max_y = 0
+
+# Parse The input
 for l in data_in.splitlines(keepends=False):
     m = re.match(r"fold along ([xy])=(\d+)", l)
     if "," in l:
@@ -54,17 +56,16 @@ for l in data_in.splitlines(keepends=False):
 
 
 for i, fold in enumerate(folds):
+    # More general way to write this I guess but easier as there are just two directions
     if fold.direction == "y":
         for y_fold in range(1, fold.value+1):
             for x in range(max_x+1):
                 input_as_matrix[fold.value-y_fold][x] = exists(input_as_matrix, x, fold.value-y_fold) \
                                               or exists(input_as_matrix, x, fold.value+y_fold)
+        # Zero out the "other" half
         for y_fold in range(fold.value, max_y+1):
             for x in range(max_x+1):
-                try:
-                    del(input_as_matrix[y_fold][x])
-                except:
-                    pass
+                input_as_matrix[y_fold][x] = 0
 
         max_y = max_y // 2
     else:
@@ -72,19 +73,17 @@ for i, fold in enumerate(folds):
             for y in range(max_y+1):
                 input_as_matrix[y][fold.value-x_fold] = exists(input_as_matrix, fold.value-x_fold, y) \
                                               or exists(input_as_matrix, fold.value+x_fold, y)
+        # Zero out the "other" half
         for x_fold in range(fold.value, max_x+1):
             for y in range(max_y+1):
-                try:
-                    del(input_as_matrix[y][x_fold])
-                except:
-                    pass
+                input_as_matrix[y][x_fold] = 0
 
         max_x = max_x // 2
+
     if i == 0:
         print("Part1=",countdots(input_as_matrix))
         assert countdots(input_as_matrix) == 675, "Wrong p1 test"
-    #print("_------------{}----------------".format(i))
-    #print_matrix(input_as_matrix, max_x, max_y)
+
 print("Part2=",countdots(input_as_matrix))
 print_matrix(input_as_matrix, max_x, max_y)
 assert countdots(input_as_matrix) == 98, "Wrong P2"
