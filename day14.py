b742d209dc77e3e14577cf3825b956e5011e0253
Diarmuid Collins <diarmuid.m.collins@gmail.com>
2021-12-14 19:19:06 +0000

    clean u[

--- day14.py
+++ day14.py
@@ -3,14 +3,16 @@ from collections import namedtuple, defaultdict
 
 Rule = namedtuple("Rule", ["matchpair", "incpair", "incpair2"])
 
+# The data can be represented as very limited number of pairs
+# when splitting a protein, you remove the original and replace
+# it with two others
+
 
 def string_to_dict(str):
-    pair_counts =  defaultdict(int)
+    pair_counts = defaultdict(int)
     for i in range(len(str) - 1):
-        if str[i:i + 2] not in pair_counts:
-            pair_counts[str[i:i + 2]] = 0
         pair_counts[str[i:i + 2]] += 1
-    return  pair_counts
+    return pair_counts
 
 
 def print_dict_pairs(pairs: dict):
@@ -31,6 +33,7 @@ def pair_count_to_counter(pair_counts):
 
 data_in = RealInput.RealInput.DAY14
 pair_counts = string_to_dict(data_in.splitlines(keepends=False)[0])
+last_letter = list(data_in.splitlines(keepends=False)[0])[-1]
 
 rules = []
 for l in data_in.splitlines(keepends=False)[2:]:
@@ -41,22 +44,17 @@ steps = 40
 for s in range(steps):
     _delta = defaultdict(int)
     for p in pair_counts:
-        if pair_counts[p] > 0:
-            for r in rules:
-                if p == r.matchpair:
-                    _delta[r.incpair] += pair_counts[p]
-                    _delta[r.incpair2] += pair_counts[p]
-                    _delta[r.matchpair] -= pair_counts[p]
+        for r in rules:
+            if p == r.matchpair and pair_counts[p] > 0:
+                _delta[r.incpair] += pair_counts[p]
+                _delta[r.incpair2] += pair_counts[p]
+                _delta[r.matchpair] -= pair_counts[p]
     for _p, change in _delta.items():
-        if pair_counts[_p] + change >= 0:
-            pair_counts[_p] += change
-        else:
-            pair_counts[_p] = 0
+        pair_counts[_p] = max(0, pair_counts[_p] + change)
 
-missing_letter = list(data_in.splitlines(keepends=False)[0])[-1]
-
+#work out answer
 pairs_counted = pair_count_to_counter(pair_counts)
-pairs_counted[missing_letter] += 1
+pairs_counted[last_letter] += 1
 counts = sorted(pairs_counted.items(), key=lambda item: item[1])
 p2_ans = abs(counts[-1][1] - counts[0][1])
 print(p2_ans)
