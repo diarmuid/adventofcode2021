719ebdcecf93940ef0b2c042df046179878394d0
Diarmuid Collins <diarmuid.m.collins@gmail.com>
2021-12-15 21:14:54 +0000

    wip p2

--- day15.py
+++ day15.py
@@ -1,6 +1,6 @@
 from queue import PriorityQueue
 import RealInput
-
+import time
 
 class Graph:
     def __init__(self, num_of_vertices):
@@ -10,9 +10,9 @@ class Graph:
 
     def add_edge(self, u, v, weight):
         self.edges[u][v] = weight
-        self.edges[v][u] = weight
+        #self.edges[v][u] = weight
 
-    def path_find(graph, start_vertex):
+    def path_find(graph, start_vertex, end_vertex=None):
         D = {v: float("inf") for v in range(graph.v)}
         D[start_vertex] = 0
 
@@ -21,6 +21,7 @@ class Graph:
 
         while not pq.empty():
             (dist, current_vertex) = pq.get()
+
             graph.visited.append(current_vertex)
 
             for neighbor in range(graph.v):
@@ -32,38 +33,57 @@ class Graph:
                         if new_cost < old_cost:
                             pq.put((new_cost, neighbor))
                             D[neighbor] = new_cost
+
         return D
 
+def wrap(i):
+    return ((i - 1) % 9) + 1
 
+st = time.time()
 data_in = RealInput.RealInput.DAY15
-max_y = len(data_in.splitlines(keepends=False))
-max_x = len(list(data_in.splitlines(keepends=False)[0]))
+lines = [[int(c) for c in l.strip()] for l in data_in.splitlines(keepends=False)]
+max_y = len(lines)
+max_x = len(lines[0])
+
+if True:  # Part 2
+    lines = [[wrap(lines[y % max_y][x % max_x] + (x // max_x) + (y // max_y)) for x in range(max_x * 5)] for y in
+             range(max_y * 5)]
+    max_x = len(lines[0])
+    max_y = len(lines)
 
 cave = Graph(max_x*max_y)
-
-input_matrix = [[] * max_x] * max_y
-for y,l in enumerate(data_in.splitlines(keepends=False)):
-    input_matrix[y] = list(map(lambda x: int(x), list(l)))
 
 point_idx = 0
 edges_added = 0
 for y in range(max_y):
     for x in range(max_x):
         _after = point_idx + 1
+        _before = point_idx - 1
         _below = point_idx + max_x
+        _above = point_idx - max_x
         if x < (max_x - 1):
-            cave.add_edge(point_idx, _after, input_matrix[y][x+1])
+            cave.add_edge(point_idx, _after, lines[y][x+1])
             edges_added += 1
         if y < (max_y - 1):
-            cave.add_edge(point_idx, _below, input_matrix[y+1][x])
+            cave.add_edge(point_idx, _below, lines[y+1][x])
+            edges_added += 1
+        if y > 0:
+            cave.add_edge(point_idx, _above, lines[y-1][x])
+            edges_added += 1
+        if x < 0:
+            cave.add_edge(point_idx, _before, lines[y][x-1])
             edges_added += 1
         point_idx += 1
+print("{:.1f}".format(time.time() - st))
 
 print("edges={}".format(edges_added))
 start_node = 0
 end_node = max_x * max_y - 1
+
 D = cave.path_find(start_node)
 end_dist = D[end_node]
-for vertex in range(len(D)):
-    print("Distance from vertex 0 to vertex", vertex, "is", D[vertex])
+#for vertex in range(len(D)):
+#    print("Distance from vertex 0 to vertex", vertex, "is", D[vertex])
 print("Distance from ", start_node," to end", end_node, "is", end_dist)
+print("{:.1f}".format(time.time() - st))
+assert end_dist==824, "Wrong p1"
